4b534a1058069ccb5da7955d699a0f2f283343c3
“wzuosen” <wzuosen@163.com>
2023-01-14 20:15:40 +0800

    【描述】:增加线程标识符文档、工具类 【提交人】:wzuosen@163.com 【日期】:2023-01-14

---
+++ thread_learn/src/main/java/cn/wzs/book/concurrentProgrammingPractice/common/annotations/ThreadSafe.java
@@ -0,0 +1,17 @@
+package cn.wzs.book.concurrentProgrammingPractice.common.annotations;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+
+/**
+ * 线程安全注解
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(value = {METHOD, TYPE})
+public @interface ThreadSafe {
+}
